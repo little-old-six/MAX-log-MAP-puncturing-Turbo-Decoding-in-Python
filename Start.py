4ae0a8f76be54797abfd48521d78846c97e4b6e6
little-old-six <119031256+little-old-six@users.noreply.github.com>
2022-12-25 10:02:10 +0800

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append(r'..\Turbo decoding (MAX-log-MAP)-（打孔）\MAX-log-MAP')
+
+from Turbo_mian import Turbo
+
+if __name__ == '__main__':
+
+    m = 2             # number of registers in this conv. code
+    sigma = 1         # Standard deviation of AWGN in channel
+    size = 100        # number of message bits
+    inter = 10        # Number of iterations
+
+    U_mes, SNR, Pe = Turbo(m, sigma, size, inter)
+    print('Turbo decoding result:', U_mes)
+    print('SNR=', SNR, '     ', 'Pe=', Pe)
+    print('over!')
+
+
